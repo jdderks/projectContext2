e0642833c344619e8aabaa4b340b6ae766abab9f
joris <j_d_derks@hotmail.com>
2021-02-16 12:29:10 +0100

    add player acceleration

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float movementSpeed = 1f;
+    float acceleration = 1f;
     // Start is called before the first frame update
     void Start()
     {
